077bf16a5419525b802ab93e6b061d6a711ca753
safaabouzaid <abouzaidsafa@gmail.com>
2024-04-29 15:51:54 +0300

    .

--- Agency/settings.py
+++ Agency/settings.py
@@ -116,11 +116,11 @@ DATABASES  = {
             'default':dj_database_url.parse('postgres://viawais1_user:MsnUtwnQZhtMgRwDoSIoPFUhVRm2jwpR@dpg-cofphgev3ddc739ngta0-a.oregon-postgres.render.com/viawais1')  
               }
 
-
+         
 
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
-
+  
 REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
