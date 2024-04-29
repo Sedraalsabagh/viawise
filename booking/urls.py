7364cd2958895f79f32e1a8e3b50e782af61a3a3
safaabouzaid <abouzaidsafa@gmail.com>
2024-04-29 13:34:43 +0300

    >

--- booking/urls.py
+++ booking/urls.py
@@ -12,5 +12,5 @@ path('make_payment/', views.make_payment, name='make_payment'),
 path('user-booking/', UserBookingsAPIView.as_view(), name='user-bookings'),
 #path('send_notification/', views.send_notification, name='send_notification'),
 path('load_seed_data/', views.load_seed_data, name='load_seed_data'),
-
+   
 ]
