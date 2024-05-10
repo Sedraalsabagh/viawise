6c18e6c98b91155d0564ff4a6be0a3b897bc5ccc
Sedraalsabagh <sedraalsabbagh2021@gmail.com>
2024-05-10 21:45:40 +0300

    .

--- flights/views.py
+++ flights/views.py
@@ -43,7 +43,7 @@ def get_by_id_flights(request,pk) :
 
 @api_view(['GET'])  
 def get_all(request) :
-   filterset=FlightsFilter(request.GET,queryset=Flight.objects.all().order_by('id'),many=True) #غيرتي من Flight to FlightSeatType
+   filterset=FlightsFilter(request.GET,queryset=Flight.objects.all().order_by('id')) #غيرتي من Flight to FlightSeatType
    serializer=FlightSerializer(filterset.qs ,many=True)
    return Response({"flights":serializer.data})
 
