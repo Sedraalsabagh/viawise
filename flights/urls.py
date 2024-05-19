d5c8c0fd8d3b6d00e0b81e2af476a949a4b40695
safaabouzaid <abouzaidsafa@gmail.com>
2024-05-19 22:14:44 +0300

    .

--- flights/urls.py
+++ flights/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 from . import views
-from .views import FlightListView,SeedDatabaseAPIView,RecommendFlightsAPIView
+from .views import FlightListView,SeedDatabaseAPIView
 
 
 urlpatterns = [
@@ -17,7 +17,7 @@ urlpatterns = [
     path('flights/',views.get_all_flights,name='flight'),
     path('recommendations1/', views.get_recommendations_user, name='get_recommendations_user'),
     #path('recommend_flights/', views.recommend_flights, name='recommend_flights'),
-    path('recommend-flights/', RecommendFlightsAPIView.as_view(), name='recommend-flights'),
+    #path('recommend-flights/', RecommendFlightsAPIView.as_view(), name='recommend-flights'),
     #path('recommendations/', get_recommendations, name='get_recommendations'),
 
 
