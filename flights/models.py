60979ad7c888b885d507d24f5f91635654055865
safaabouzaid <abouzaidsafa@gmail.com>
2024-05-20 18:49:19 +0300

    ADD fligth description

--- flights/models.py
+++ flights/models.py
@@ -113,6 +113,9 @@ class Flight(models.Model):
     destination_type=models.CharField(max_length=100,choices=DESTIATION_CHOICES,blank=True, null=True)
     destination_climate = models.CharField(max_length=100,choices=CLIMATE_CHOICES, blank=True, null=True)
     flight_schedule = models.CharField(max_length=100, choices=FLIGHT_SCHEDULE_CHOICES, blank=True, null=True)
+
+    description = models.TextField(max_length=2000, default="", blank=True,null=True)
+
     def __str__(self): 
         return str(self.id)
 
