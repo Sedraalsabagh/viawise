a7f2105e06b24d170394c38179247a243cb57106
Sedraalsabagh <sedraalsabbagh2021@gmail.com>
2024-05-24 19:21:29 +0300

    .

--- booking/views.py
+++ booking/views.py
@@ -380,8 +380,6 @@ def all_booking(request):
 
 
 
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
 
 
 @api_view(['POST'])
