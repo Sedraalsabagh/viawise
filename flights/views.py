0c543dae735edd94472b8324b290198e806784d1
safaabouzaid <abouzaidsafa@gmail.com>
2024-05-24 22:52:49 +0300

    .

--- flights/views.py
+++ flights/views.py
@@ -772,11 +772,9 @@ from rest_framework.permissions import IsAuthenticated
 from django.http import JsonResponse
 from .views import recommendations_user, get_recommendations
 
-
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def recommendations_combined(request):
-
     django_request = HttpRequest()
     django_request.user = request.user
     django_request.method = 'GET'
@@ -784,8 +782,18 @@ def recommendations_combined(request):
     user_response = recommendations_user(django_request)
     flight_response = get_recommendations(django_request)
 
-    user_recommendations = json.loads(user_response.content).get("recommendations", []) if isinstance(user_response, JsonResponse) else user_response
-    flight_recommendations = json.loads(flight_response.content).get("recommendations", []) if isinstance(flight_response, JsonResponse) else flight_recommendations
+    user_recommendations = []
+    flight_recommendations = []
+    
+    if isinstance(user_response, JsonResponse):
+        user_recommendations = json.loads(user_response.content).get("recommendations", [])
+    else:
+        user_recommendations = user_response
+    
+    if isinstance(flight_response, JsonResponse):
+        flight_recommendations = json.loads(flight_response.content).get("recommendations", [])
+    else:
+        flight_recommendations = flight_response
 
     combined_recommendations = {frozenset(item.items()): item for item in user_recommendations + flight_recommendations}
     unique_recommendations = list(combined_recommendations.values())
