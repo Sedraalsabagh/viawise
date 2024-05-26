0be8d7797973ae7da394bfae4bc794ab1f9fe1fb
Sedraalsabagh <sedraalsabbagh2021@gmail.com>
2024-05-26 11:18:27 +0300

    .

--- hotel/urls.py
+++ hotel/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
      path('hotel/<int:star_rating>/',views.hotel, name='hotel_by_rating'),
      path('hotel/',views.hotel_list,name='hotel_list') ,
      path('hotelfilter/',views.hotels_filter,name='hotels_filter'),
-     path('hotel_details/',views.hotel_details,name='hotel_details')
+     path('hotel_details/<int:id>/',views.hotel_details,name='hotel_details')
 
          ]
 
